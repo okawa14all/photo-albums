a4a8647e2ca5acd59a130efd3277d135421a101c
Hitoshi Okawa <hitoshi.okawa.biz@gmail.com>
2019-06-11 09:38:03 +0900

    Add AlbumLinst component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
-import Amplify from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react';
+import {
+  Grid,
+  Header,
+  Input,
+  List,
+  Segment
+} from 'semantic-ui-react';
+import Amplify, { graphqlOperation } from 'aws-amplify';
+import {
+  withAuthenticator,
+  Connect
+} from 'aws-amplify-react';
 import aws_exports from './aws-exports';
 import './App.css';
 
@@ -28,6 +37,40 @@ const signUpConfig = {
     },
   ]
 };
+
+const makeComparator = (key, order = 'asc') => {
+  return (a, b) => {
+    if(!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+    const aVal = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
+    const bVal = (typeof b[key] === 'string') ? b[key].toUpperCase() : b[key];
+    let comparison = 0;
+    if (aVal > bVal) comparison = 1;
+    if (aVal < bVal) comparison = -1;
+    return order === 'desc' ? (comparison * -1) : comparison
+  };
+};
+
+interface AlbumData {
+  id: string;
+  name: string;
+}
+
+type TAlbumsListProps = {
+  albums: Array<AlbumData>
+}
+
+const AlbumsList: React.FC<TAlbumsListProps> = ({ albums }: TAlbumsListProps) => {
+  <Segment>
+    <Header as='h3'>My Albums</Header>
+    <List divided relaxed>
+      {albums.sort(makeComparator('name')).map(album =>
+        <li key={album.id}>
+          {album.name}
+        </li>
+      )}
+    </List>
+  </Segment>
+}
 
 const App: React.FC = () => {
   return (
